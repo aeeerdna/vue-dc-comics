ce70754edb0d6ae93acbb851fa1c8af6bf19b433
Andrea Miglietta <m.andrea96@hotmail.it>
2022-09-29 18:43:30 +0200

    impostazione footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>footer</div>
+  <div>
+    <FooterECommerceComponent />
+    <FooterLinkComponent />
+    <FooterSocialComponent />
+  </div>
 </template>
 
 <script>
+import FooterECommerceComponent from "@/components/FooterECommerceComponent.vue";
+import FooterLinkComponent from "@/components/FooterLinkComponent.vue";
+import FooterSocialComponent from "@/components/FooterSocialComponent.vue";
 export default {
   name: "FooterComponent",
+  components: {
+    FooterECommerceComponent,
+    FooterLinkComponent,
+    FooterSocialComponent,
+  },
 };
 </script>
 
---
+++ src/components/FooterECommerceComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>eCommerce</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
